8bd5714aab9a8820908d282e0a6bf40777eb3947
Alexandr Axenov <alexanderaxenov@mail.ru>
2020-01-28 20:15:13 +0800

    Updates

---
+++ precache-manifest.c0705110196ef43b9538898398877dae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7672795dcf28b44d254c1461783a457",
+    "url": "/react-training/build/index.html"
+  },
+  {
+    "revision": "cd3956300f281b7944ab",
+    "url": "/react-training/build/static/css/main.13efc9cf.chunk.css"
+  },
+  {
+    "revision": "ff3c3d37c495072b6795",
+    "url": "/react-training/build/static/js/2.f08b355c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-training/build/static/js/2.f08b355c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40d0e172f5921990072b",
+    "url": "/react-training/build/static/js/3.4c4c3b0b.chunk.js"
+  },
+  {
+    "revision": "cd3956300f281b7944ab",
+    "url": "/react-training/build/static/js/main.501d1eb1.chunk.js"
+  },
+  {
+    "revision": "0767849b42287f0a27cf",
+    "url": "/react-training/build/static/js/runtime-main.96a9187f.js"
+  }
+]);
